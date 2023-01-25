80f54348b7046c8cd325e79b8b0ac8bb3876a839
Dominik K <dominik@koch-bautechnik.de>
2023-01-25 18:27:39 +0100

    Changed file structure

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+require('ejs');
+
+app.set('view engine', 'ejs');
+app.use('/static', express.static('static'));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
+app.get('/invite', (req, res) => {
+    res.redirect('https://discord.com/oauth2/authorize?client_id=1057043734526701589&permissions=2415938576&scope=bot%20applications.commands');
+})
+
+app.get('/support', (req, res) => {
+    res.redirect('https://discord.gg/vanityroles');
+})
+
+app.listen(4829, () => console.log('http://localhost:4829'));
